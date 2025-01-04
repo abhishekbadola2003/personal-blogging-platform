2cf10e0579d6966f1220ea01812018d398e156eb
Abhishek-Badola <abhishekbadola28@gmail.com>
2025-01-04 21:04:17 +0530

    .env

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import notes_routes from "../src/routes/notes_routes";
 import mongoose, { Mongoose } from "mongoose";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use("/notes", notes_routes);
 
 app.listen(4000, () => {
-  console.log("server started running on port 4000");
+  console.log(`server started running on port 4000`);
 });
 
 // app.get("/", (req, res) => {
 //   res.send("server running successfullly");
 // });
-
-const mongo_uri =
-  "mongodb+srv://abhishekbadola28:BRW2DTg8wfAw8pbY@backenddb.pufgh.mongodb.net/";
-mongoose.Promise;
-mongoose.connect(mongo_uri);
-mongoose.connection.on("error", (error: Error) => console.log(error));
