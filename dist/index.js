722c44a353523d98ff55219086b74242779f66bc
Abhishek-Badola <abhishekbadola28@gmail.com>
2025-01-07 12:31:32 +0530

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
 const notes_routes_1 = __importDefault(require("../src/routes/notes_routes"));
 const mongoose_1 = __importDefault(require("mongoose"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use("/notes", notes_routes_1.default);
-app.listen(4000, () => {
-    console.log("server started running on port 4000");
+app.use("/api/notes", notes_routes_1.default);
+//connect to db with error handling
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect(process.env.MONGO_URI || "");
+        console.log("Database connected successfully");
+    }
+    catch (error) {
+        console.log("connection to database failed.");
+        process.exit(1);
+    }
+}))();
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`server started running on ${PORT} `);
+    console.log("Connected to MongoDb ");
 });
-// app.get("/", (req, res) => {
-//   res.send("server running successfullly");
-// });
-const mongo_uri = "mongodb+srv://abhishekbadola28:BRW2DTg8wfAw8pbY@backenddb.pufgh.mongodb.net/";
-mongoose_1.default.Promise;
-mongoose_1.default.connect(mongo_uri);
-mongoose_1.default.connection.on("error", (error) => console.log(error));
